d72742f818cb4b68b956f35450631be747e5177e
gelexxi <schew4uk98@gmail.com>
2023-09-28 18:37:18 +0300

    Creadet sidebar add popular post and tags

--- siteblog/blog/urls.py
+++ siteblog/blog/urls.py
@@ -5,6 +5,7 @@ from .views import *
 urlpatterns = [
     path('', Home.as_view(), name='home'),
     path('category/<str:slug>/', PostByCategory.as_view(), name='category'),
+    path('tag/<str:slug>/', PostsByTag.as_view(), name='tag'),
     path('post/<str:slug>/', GetPost.as_view(), name='post'),
 
 ]
--- siteblog/blog/views.py
+++ siteblog/blog/views.py
@@ -8,6 +8,7 @@ class Home(ListView):
     context_object_name = 'posts'
     paginate_by = 4
 
+
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Classic Blog Design'
@@ -37,6 +38,20 @@ class GetPost(DetailView):
         self.object.views = F('views')+1
         self.object.save()
         self.object.refresh_from_db()
+        return context
+
+class PostsByTag(ListView):
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 4
+    allow_empty = False
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['slug'])
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "Записи по тегу " + str(Tag.objects.get(slug=self.kwargs['slug']))
         return context
 # def index(request):
 #     return render(request, 'blog/index.html')
